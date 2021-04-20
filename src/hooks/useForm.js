77bc6ccd0511ec2c339a9c5c1f8ed2d3a2bacbf0
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-20 17:56:51 -0500

    add persist feature

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -13,12 +13,27 @@ export const useForm = (initialState = {}) => {
       .then((data) => setValues(v => ({ ...v, departments: data })));
   }
 
+  // FETCH DEPARTMENTS
   useEffect(() => {
     fetchDepartments();
   }, []);
 
+  // CHECK LOCALSTORAGE AND ASSIGN VALUES TO STATE
+  useEffect(() => {
+    const stateSaved = JSON.parse(localStorage.getItem("teleconsulta"))
+    if (!stateSaved) {
+      localStorage.setItem("teleconsulta", JSON.stringify(initialState))
+    }
+    else {
+      setValues(stateSaved)
+      // console.log('setting state')
+    }
+    // console.log(stateSaved)
+  }, [initialState])
+
   const reset = () => {
     setValues({ ...initialState });
+    localStorage.setItem("teleconsulta", JSON.stringify(initialState))
     fetchDepartments();
   }
 
@@ -30,6 +45,7 @@ export const useForm = (initialState = {}) => {
       [target.name]: target.value
     });
 
+    localStorage.setItem("teleconsulta", JSON.stringify({ ...values, [target.name]: target.value }))
   }
 
   return [values, handleInputChange, reset];
