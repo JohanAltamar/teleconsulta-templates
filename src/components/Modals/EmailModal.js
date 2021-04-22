a6ccc7aa3db8fb9d16d59dddd0852567d668a9cc
Johan Altamar <johan.altamar.dev@gmail.com>
2021-04-22 17:08:55 -0500

    update send message function

--- src/components/Modals/EmailModal.js
+++ src/components/Modals/EmailModal.js
@@ -8,25 +8,44 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import { AppContext } from '../../context/AppContext';
 import DropZone from '../Uploads/DropZone'
 import sendMail from '../../helpers/sendMail';
+import Swal from 'sweetalert2';
+import { useHistory } from 'react-router';
 
 const EmailModal = ({ open, handleClose }) => {
+  const history = useHistory();
   const [files, setFiles] = useState([])
 
   const { formValue, handleInputChange, resetSomeFields } = useContext(AppContext);
-  const { email, message = "Buen día \nadjunto le envío", subject = "" } = formValue;
+  const { email, message, subject, from, appPassword } = formValue;
 
   const handleSendMessage = async () => {
-    const mailInfo = {
-      to: email,
-      from: "TELECONSULTA@gmail.com",
-      subject,
-      text: message
-    }
-    const data = await sendMail(files, mailInfo)
-    if (data) {
-      setFiles([]);
-      resetSomeFields("subject", "message")
-      handleClose();
+    try {
+      const mailInfo = {
+        to: email,
+        from,
+        password: appPassword,
+        subject,
+        text: message
+      }
+      const data = await sendMail(files, mailInfo)
+
+      if (data) {
+        setFiles([]);
+        resetSomeFields("subject", "message")
+        handleClose();
+      }
+    } catch (error) {
+      if (error.response) {
+        const { data } = error.response;
+        if (data?.code === "EAUTH") {
+          Swal.fire("Error", "Credenciales de CORREO erróneas, ingresalas nuevamente", "error").then(() => {
+            history.push("/login")
+          })
+        }
+      }
+      else {
+        Swal.fire("Error", error.message, "error")
+      }
     }
   }
 
--- src/helpers/sendMail.js
+++ src/helpers/sendMail.js
@@ -2,22 +2,19 @@ import axios from "axios";
 import Swal from "sweetalert2";
 
 const sendMail = async (files, mailInfo) => {
-  try {
-    const data = new FormData()
+  const data = new FormData()
 
-    files.forEach(file => {
-      data.append(file.name, file)
-    })
+  files.forEach(file => {
+    data.append(file.name, file)
+  })
 
-    data.append('mailInfo', JSON.stringify({ ...mailInfo, from: "teleorientacion.sura.21@gmail.com", password: "nyloltgekacrjkoj", from2: `Teleorientación SURA <teleorientacion.sura.21@gmail.com>` }))
+  data.append('mailInfo', JSON.stringify({ ...mailInfo, from2: `Teleorientación SURA <${mailInfo.from}>` }))
 
-    const res = await axios.post(`http://localhost:8080/api/mailer`, data)
-    // const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/mailer`, data)
-    Swal.fire("Éxito", res.data.msg, "success")
-    return res.data
-  } catch (error) {
-    Swal.fire("Error", error.message, "error")
-  }
+  // const res = await axios.post(`http://localhost:8080/api/mailer`, data)
+  const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/mailer`, data)
+  Swal.fire("Éxito", res.data.msg, "success")
+  return res.data
+
 }
 
 export default sendMail;
